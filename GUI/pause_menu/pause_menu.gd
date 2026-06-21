57d7d720fc5adf99cd42cc944466102c139122e8
Elvis <elvis@casaio.net>
2026-06-21 16:01:10 +0200

    tuto 16 - ajout du systeme d'inventaire

--- GUI/pause_menu/pause_menu.gd
+++ GUI/pause_menu/pause_menu.gd
@@ -1,7 +1,11 @@
 extends CanvasLayer
 
-@onready var button_save: Button = $VBoxContainer/Button_Save
-@onready var button_load: Button = $VBoxContainer/Button_Load
+signal shown
+signal hidden
+
+@onready var button_save: Button = $Control/HBoxContainer/Button_Save
+@onready var button_load: Button = $Control/HBoxContainer/Button_Load
+@onready var item_description: Label = $Control/ItemDescription
 
 var is_paused: bool = false
 
@@ -28,13 +32,14 @@ func show_pause_menu() -> void:
 	get_tree().paused = true
 	visible = true
 	is_paused = true
-	button_save.grab_focus()
+	shown.emit()
 
 
 func hide_pause_menu() -> void:
 	get_tree().paused = false
 	visible = false
 	is_paused = false
+	hidden.emit()
 
 
 func _on_save_pressed() -> void:
@@ -54,3 +59,7 @@ func _on_load_pressed() -> void:
 	await LevelManager.level_load_started
 	hide_pause_menu()
 	pass
+
+
+func update_item_description(new_text: String) -> void:
+	item_description.text = new_text
